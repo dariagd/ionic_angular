ec18f8be5c765dc00f7f3682c9e6753628d19636
daryagd <grishinadaryadm@gmail.com>
2022-01-03 09:36:25 +0300

    class ion-padding and div class scss

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( public actionSheetCtrl: ActionSheetController) { }
 
+  async openMenu(){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Update Album',
+      buttons: [{
+        text: 'Destructive',
+        role: 'destructive',
+        handler: () => {
+          console.log('Destructive clicked');}
+      },{
+        text: 'Create',
+        handler: () => {
+          console.log('Create clicked');
+        }
+      },{
+        text: 'Play',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      },{
+        text: 'Favorite',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      },{
+        text: 'Cancel',
+        role: 'clicked',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+      ]
+    });
+    await actionSheet.present();
+  }
 }
